4bcb51a8aa9045cbb35e5eadff15e2507f830caf
asmakazi515 <asmakazi5841@gmail.com>
2024-08-09 15:25:30 +0530

    first

---
+++ vowelif.py
@@ -0,0 +1,6 @@
+x=str(input("enter alphabet:"))
+if x=="a" or x=="e" or x=="i" or x=="u" or x=="o" or x=="A" or x=="E" or x=="I" or x=="O" or x=="U":
+    print(f"{x} is a vowel")
+else:
+    print(f"{x} is consonent")
+
